6e1904fdfa39302b237f7e73ad18c45ecd027c98
Joshua H <65923075+deadorbit@users.noreply.github.com>
2024-03-22 14:00:02 -0400

    Update MyScheduler.java

--- MyScheduler.java
+++ MyScheduler.java
@@ -2,6 +2,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.*;
 
+/**
+ * Implements a multi-token semaphore.
+ * 
+ * @author: Ella Borgin, Jim Stock
+ */
+
 public class MyBlockingQueue<T> {
 
 }
