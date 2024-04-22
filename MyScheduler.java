34b64e8d4068263d570b31cb2bead2da00f0ee1e
Joshua H <65923075+deadorbit@users.noreply.github.com>
2024-04-22 13:45:49 -0400

    Updated to latest

--- MyScheduler.java
+++ MyScheduler.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -11,10 +12,10 @@ public class MyScheduler {
     int numJobs;
     String property;
 
-    int avgWaitTime;
-    int maxWait;
-    int combined;
-    int deadlines;
+    int avgWaitTime = 0;
+    int maxWait = 0;
+    int combined = 0;
+    ArrayList<Long> deadlines = new ArrayList<Long>();
 
     LinkedBlockingQueue<Job> outQueue;
     LinkedBlockingQueue<Job> inQueue;
@@ -43,6 +44,52 @@ public class MyScheduler {
         return this.inQueue;
     }
 
+    public LinkedBlockingQueue<Job> scheudlingAlgorithm(LinkedBlockingQueue<Job> Jobs) {
+        ArrayList<Job> tempArray = new ArrayList<Job>(); // Temporary Array
+        LinkedBlockingQueue<Job> tempQueue = new LinkedBlockingQueue<Job>(numJobs);
+        this.deadlines.clear();
+
+        for (Job s : Jobs)
+            tempArray.add(s);
+
+        // Do math to get statistics
+        for (int i = 0; i <= tempArray.size(); i++) {
+            this.numJobs++;
+
+            this.avgWaitTime += tempArray.get(i).getWaitTime();
+            deadlines.add(tempArray.get(i).getDeadline());
+        }
+
+        // Use statistics to predict which algorithm needs to be used
+        this.property = "SDF";
+
+        switch (this.property) {
+            case "SDF":
+                tempArray = shortestDeadlineFirst(tempArray);
+                break;
+
+            default:
+                break;
+        }
+
+        for (int i = 0; i <= tempArray.size(); i++) {
+
+        }
+
+        return tempQueue;
+
+    }
+
+    public ArrayList<Job> shortestDeadlineFirst(ArrayList<Job> Jobs) {
+        Comparator<Job> sorter = new Comparator<Job>() {
+            public int compare(Job left, Job right) {
+                return Math.toIntExact(left.getDeadline() - right.getDeadline());
+            }
+        };
+        Jobs.sort(sorter);
+        return Jobs;
+    }
+
     /*
      * You will take jobs from the incoming queue (and probably store them), and
      * You will put jobs on the outgoing queue in the order of your choosing.
@@ -52,7 +99,7 @@ public class MyScheduler {
             try {
                 Job job = inQueue.poll();
                 // System.out.println(job.getLength());
-                // Thread.sleep(job.getLength());
+                scheudlingAlgorithm(inQueue);
                 outQueue.put(job);
                 numJobs--;
             } catch (Exception e) {
